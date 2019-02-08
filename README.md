3d9f8101866f2af31a31bb44f88301bad2d8afa6
Tiago Resende <tiagor87@gmail.com>
2019-02-08 14:46:00 -0200

    Cria console app para encriptar arquivos escutando mudanças em uma pasta.

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# criptographer
+# Encryptor
+
+Sistema para encriptar arquivos de forma fácil utilizando o padrão
+Pretty Good Privacy (PGP).
+
+* Crie um arquivo na pasta do executável com o nome __PublicKey__,
+o conteúdo do arquivo deverá ser como no exemplo abaixo:
+```
+-----BEGIN PGP PUBLIC KEY BLOCK-----
+Version: BCPG C# v1.6.1.0
+
+mQENBFxdsKMBCACRLct0nwmx2HjswG6qjJne8xqKCJltir9K7rKvxieOIjCw1QFP
+Dlv4xQbQIM8eoRxnJJGB15b764o9x376gofGLffETZL83RgfFn4sMO04oAqBGKzI
+CZCnxr3O9FvCZsFi5noCjP7iSSivwHo/dWRQMCbnC49NNrhiRoudOVtNSOxIInZI
+4WlnytAxW64BXNL7xm5qxGVUiadvLk3yEh5VaGoot2dg6N1edYWWRr0EZw86LlPp
+8kyc2So8MJSCpQuKysbWQ8xJFfSf9VraNCSv+0QaMRu7CakXdG+ZOMuuZG5jpkB5
+jWyjSO80fvqte4Xkkyj6qn4DFph/3yOmbL3nABEBAAG0AIkBHAQQAQIABgUCXF2w
+owAKCRAcdhJ8khzkpN3qB/sHTE7yg3rKjBxlf8oshs0YHKgHS6MTq88A2ZWxGfxZ
+BYrOjOpqWTsDxiRdaIiuSKUarCg2mwdxsbpkQcgY98arL8PPcW9SYMMqkhaRPB6U
+FjAlRdRLxiXu1Jldf8/Rd+E3yhCcrt4YGP7A0B1tYtLwCrXXJ+5oyi0hxhuKv8jN
+4nVPbLuJoGppluPpkUjQBEYBz2CZO4kpYQw49Dh4hlpPMmKzLU+g/AEpNKRwPyxh
+IxsEitwU9vuDGRyXu7Wc01G9WLPMXAylpPQMqKVj+aPX2hexV5LhaSxH1h7jUGTl
+XadsnnbNEHWv2WqK16mPg3uOdwMpIaGoPyFuTGhGHFqu
+=RsCq
+-----END PGP PUBLIC KEY BLOCK-----
+
+```
+* Coloque o arquivo que deseja criptografar na pasta __Input__;
+    * O aplicativo detectará a inclusão do arquivo.
+* Aguarde a aplicação finalizar o processo;
+* Obtenha o arquivo encriptado na pasta __Output__.
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Caching;
+using System.Threading;
+using PgpCore;
+
+namespace Encryptor.ConsoleApp
+{
+    class Program
+    {
+        private static string _keyPath = Path.Combine(Directory.GetCurrentDirectory(), "PublicKey");
+        private static string _inputDir = Path.Combine(Directory.GetCurrentDirectory(), "Input");
+        private static string _outputDir = Path.Combine(Directory.GetCurrentDirectory(), "Output");
+        static void Main(string[] args)
+        {
+            if (Directory.Exists(_inputDir) == false)
+            {
+                Console.WriteLine($"Creating input directory: \"{_inputDir}\".");
+                Directory.CreateDirectory(_inputDir);
+            }
+
+            if (Directory.Exists(_outputDir) == false)
+            {
+                Console.WriteLine($"Creating output directory: \"{_outputDir}\".");
+                Directory.CreateDirectory(_outputDir);
+            }
+
+            while (File.Exists(_keyPath) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"Please, put the public key in \"{_keyPath}\".");
+                Console.ForegroundColor = ConsoleColor.White;
+                Wait("Press a key to continue...");
+            }
+
+            var cache = MemoryCache.Default;
+            
+            using (var fileWatcher = new FileSystemWatcher())
+            {
+                Console.WriteLine($"Listening to file changes in \"{_inputDir}\".");
+                fileWatcher.Path = _inputDir;
+                fileWatcher.NotifyFilter = NotifyFilters.LastWrite;
+                fileWatcher.Filter = "*.*";
+                fileWatcher.Changed += (source, @event) =>
+                {
+                    Console.WriteLine($"Detected file \"{@event.FullPath}\"");
+                    cache.AddOrGetExisting(@event.FullPath, @event,
+                        new CacheItemPolicy
+                        {
+                            AbsoluteExpiration = DateTime.UtcNow.AddSeconds(10),
+                            RemovedCallback = EncryptFile
+                        });
+                };
+                fileWatcher.IncludeSubdirectories = true;
+                fileWatcher.EnableRaisingEvents = true;
+                Wait("Press a key to exit...");
+            } 
+        }
+
+        static void EncryptFile(CacheEntryRemovedArguments arguments)
+        {
+            if (arguments.RemovedReason != CacheEntryRemovedReason.Expired)
+            {
+                return;
+            }
+
+            var @event = (FileSystemEventArgs) arguments.CacheItem.Value;
+            Console.WriteLine($"Encrypting file \"{@event.FullPath}\".");
+            try
+            {
+                using (var pgp = new PGP())
+                {
+                    var output = @event.FullPath.Replace(_inputDir, _outputDir);
+
+                    pgp.EncryptFile(
+                        @event.FullPath,
+                        output,
+                        _keyPath);
+
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine($"File encrypted to \"{output}\" with \"{_keyPath}\".\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"Failed to encrypt file \"{@event.FullPath}\".\nError message: \"{ex.Message}\"");
+            }
+            finally
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Press a key to exit...");
+            }
+        }
+
+        static void Wait(string message)
+        {
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
